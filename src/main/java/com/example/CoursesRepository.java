861634648a5196cebe8cd91836847dd8ea94f58a
Atish-Jain <83014070+Atish-Jain@users.noreply.github.com>
2021-06-30 12:27:34 +0530

    Update CoursesRepository.java

--- src/main/java/com/example/CoursesRepository.java
+++ src/main/java/com/example/CoursesRepository.java
@@ -2,6 +2,7 @@ package com.example;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+//Jpa Repository for perfroming CRUD operations in DB
 public interface CoursesRepository extends JpaRepository<Courses,Integer>{
 
 }
