03165b0b7a3d73b965c5ad4722741ef9afde6716
Atish-Jain <83014070+Atish-Jain@users.noreply.github.com>
2021-06-30 12:26:31 +0530

    Update GlobalExceptions.java

--- src/main/java/com/example/GlobalExceptions.java
+++ src/main/java/com/example/GlobalExceptions.java
@@ -9,18 +9,21 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class GlobalExceptions 
 {
 
+	//When User enter id whcih does not has any user corresponding to it then this exception is called
 	@ExceptionHandler(NoUserFoundException.class)
-		public ResponseEntity<String> NoUser(NoUserFoundException exception)
+		public ResponseEntity<String> NoUser(NoUserFoundException exception) 
 		{	  
 			  return new ResponseEntity<String> (exception.getMessage(),HttpStatus.NOT_FOUND);
 		}
 	
+	//When User enter does not enter name during POST API then this exception is called
 	@ExceptionHandler(NoNameException.class)
 	public ResponseEntity<String> NoName(NoNameException exception)
 	{	  
 		  return new ResponseEntity<String> (exception.getMessage(),HttpStatus.NOT_ACCEPTABLE);
 	}
 	
+	//Global Exception
 	@ExceptionHandler(Exception.class)
 		public HttpStatus globalException()
 		{
