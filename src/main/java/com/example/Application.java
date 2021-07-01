04df244f633a630dd410b53476869e401c7262ce
Atish-Jain <83014070+Atish-Jain@users.noreply.github.com>
2021-07-01 22:45:44 +0530

    Update Application.java

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaRepositories(basePackages = "com.example")
+@EnableJpaRepositories(basePackages = "com.example")//Enabling Jpa Repository in the project
 @SpringBootApplication
 public class Application {
 
